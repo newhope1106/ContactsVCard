64e87cdc5d5e2c99c2fbf123d69723a40afaffd9
newhope1106 <408705420@qq.com>
2014-01-19 13:15:22 +0800

    将常用方法用工具类中的方法代替

--- src/com/newhope/contactsvcard/ContactsVCardActivity.java
+++ src/com/newhope/contactsvcard/ContactsVCardActivity.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
+import com.newhope.contactsvcard.utils.CharacterEncodingConverter;
+
 import android.os.Bundle;
 import android.os.Environment;
 import android.annotation.SuppressLint;
@@ -173,83 +175,13 @@ public class ContactsVCardActivity extends Activity {
 		while(secondNameLength >= 0){
 			valueUnicode = startIndex + (int)(Math.random()*range);
 			String unicodeName = "\\u" + Integer.toHexString(valueUnicode).toUpperCase();
-			secondName.append(unicodeToUtf8(unicodeName));
+			secondName.append(CharacterEncodingConverter.unicodeToUtf8(unicodeName));
 			
 			secondNameLength --;
 		}
 		
 		return secondName.toString();
 	}
-	
-	/**
-    *
-    * @param theString
-    * @return String
-    */
-   public static String unicodeToUtf8(String theString) {
-       char aChar;
-       int len = theString.length();
-       StringBuffer outBuffer = new StringBuffer(len);
-       for (int x = 0; x < len;) {
-           aChar = theString.charAt(x++);
-           if (aChar == '\\') {
-               aChar = theString.charAt(x++);
-               if (aChar == 'u') {
-                   // Read the xxxx
-                   int value = 0;
-                   for (int i = 0; i < 4; i++) {
-                       aChar = theString.charAt(x++);
-                       switch (aChar) {
-                       case '0':
-                       case '1':
-                       case '2':
-                       case '3':
-                       case '4':
-                       case '5':
-                       case '6':
-                       case '7':
-                       case '8':
-                       case '9':
-                           value = (value << 4) + aChar - '0';
-                           break;
-                       case 'a':
-                       case 'b':
-                       case 'c':
-                       case 'd':
-                       case 'e':
-                       case 'f':
-                           value = (value << 4) + 10 + aChar - 'a';
-                           break;
-                       case 'A':
-                       case 'B':
-                       case 'C':
-                       case 'D':
-                       case 'E':
-                       case 'F':
-                           value = (value << 4) + 10 + aChar - 'A';
-                           break;
-                       default:
-                           throw new IllegalArgumentException(
-                                   "Malformed   \\uxxxx   encoding.");
-                       }
-                   }
-                   outBuffer.append((char) value);
-               } else {
-                   if (aChar == 't')
-                       aChar = '\t';
-                   else if (aChar == 'r')
-                       aChar = '\r';
-                   else if (aChar == 'n')
-                       aChar = '\n';
-                   else if (aChar == 'f')
-                       aChar = '\f';
-                   outBuffer.append(aChar);
-               }
-           } else
-               outBuffer.append(aChar);
-       }
-       return outBuffer.toString();
-   }
 	
 	private class OnSubmitBtnClickListener implements OnClickListener{
 		private Context mContext; 
